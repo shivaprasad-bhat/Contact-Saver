a2ccda4b2f77171c2f8d73761296e3e158649fbf
AmanKamani <kamaniamanak@gmail.com>
2021-07-10 16:19:54 +0530

    Feature: Created UI Screens.
    - Created Home screen with RecyclerView & Floating Action Button.
    - Created layout for recycler view list item.
    - Created Add data form.
    
    closes #1

--- app/src/main/java/com/jb/svb/contactsaver/ContactActivity.kt
+++ app/src/main/java/com/jb/svb/contactsaver/ContactActivity.kt
@@ -1,11 +1,11 @@
 package com.jb.svb.contactsaver
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class ContactActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_contact)
     }
 }
