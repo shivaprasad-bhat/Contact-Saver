b8f4a314e765500c149f41a9a26eea59691112ae
Shivaprasad Bhat <svbhat.neelmane@gmail.com>
2021-07-11 00:21:02 +0530

    Adding Launch Config, Theme, Icon, Navigation and Single Event
    - Extended SingleLiveEvent
    - Changed the app icon
    - Changed the them to light always
    - Set up the Navigation Component
    - Set up the Launch

--- app/src/main/java/com/jb/svb/contactsaver/ui/HomeFragment.kt
+++ app/src/main/java/com/jb/svb/contactsaver/ui/HomeFragment.kt
@@ -1,6 +1,35 @@
 package com.jb.svb.contactsaver.ui
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.jb.svb.contactsaver.R
+import com.jb.svb.contactsaver.databinding.FragmentHomeBinding
+import com.jb.svb.contactsaver.viewmodels.HomeViewModel
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(R.layout.fragment_home) {
+    private lateinit var binding: FragmentHomeBinding
+    private val viewModel by lazy {
+        ViewModelProvider(this).get(HomeViewModel::class.java)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentHomeBinding.bind(view)
+
+        binding.apply {
+            homeViewModel = viewModel
+            lifecycleOwner = this@HomeFragment.viewLifecycleOwner
+        }
+
+        viewModel.navigateToCreateContact.observe(viewLifecycleOwner) {
+            findNavController().navigate(
+                HomeFragmentDirections.actionHomeFragmentToNewContactFragment(
+                )
+            )
+        }
+    }
+
 }
