55080fbcef06331565c21158ef77fa02d53dfcd4
AmanKamani <kamaniamanak@gmail.com>
2021-08-22 00:41:48 +0530

    feat(implement-custom-response): Applied custom response to database calls
    - added long return type for Contact insert.
    - wrapped database response with CustomResponse in repository.
    - observing the response in fragment and based on that showing success or error message.
    
    Signed-off-by: AmanKamani <kamaniamanak@gmail.com>

--- app/src/main/java/com/jb/svb/contactsaver/persistance/dao/ContactDao.kt
+++ app/src/main/java/com/jb/svb/contactsaver/persistance/dao/ContactDao.kt
@@ -1,13 +1,18 @@
 package com.jb.svb.contactsaver.persistance.dao
 
-import androidx.room.*
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
 import com.jb.svb.contactsaver.persistance.entities.ContactEntity
 
 @Dao
 interface ContactDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insert(contact: ContactEntity)
+    suspend fun insert(contact: ContactEntity): Long
 
     @Update
     suspend fun update(contact: ContactEntity)
